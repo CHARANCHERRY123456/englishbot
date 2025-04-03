3dbbced858d6cf624e79d8770ae68d57feb83f40
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-04-03 23:39:36 +0530

    remove config

--- config.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    SECRET_KEY = "this-is-a-secret-key" 
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
-    MONGODB_URL = "mongodb://localhost:27017/" 
-    GEMINI_API_KEY = "AIzaSyCc6a8h-qEPjJF5cJ6miqrnKRrSzwOS24U"
-
-settings = Settings()
